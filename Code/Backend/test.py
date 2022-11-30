e66229d62b60de0e8feb46e4e71af38574ddd116
Jonathan <40543169+jonathanli15@users.noreply.github.com>
2022-11-29 20:11:49 -0500

    Update test.py

--- Code/Backend/test.py
+++ Code/Backend/test.py
@@ -1,42 +1,66 @@
 import ds4se.facade as facade
 import os
 import sys
-import ast
 
 def new_probability(num1, num2):   #written as a function to be more easily updated to a different algorithm later
     return (num1+num2)/2
+    
+def traceabilityResult(source, target, targetFile, feedback, model, metric = None):
+    with open(os.path.join(target, targetFile), 'r', encoding='latin1') as f: # open in readonly mode
+        targetData = f.read()
+        f.close()
+    values = {}
+    for sourceFilename in os.listdir(source):
+        with open(os.path.join(source,sourceFilename), 'r', encoding='latin1') as f:
+            sourceData = f.read()
+            f.close()
+        if metric is None:
+            result = facade.TraceLinkValue(sourceData,targetData, model)
+        else:
+            result = facade.TraceLinkValue(sourceData,targetData, model, metric)
+
+        traceResult = result[1]
+        tmpStr = targetFile+" "+sourceFilename
+        if tmpStr in input:  #check if the file pair is in the input from the user
+            traceResult = new_probability(traceResult, float(feedback[tmpStr]))  #recalculating the probability
+
+        # print("Source File: ",sourceFilename, "Target File: ", targetFilename, "Traceability: ",result)
+
+    # new code added to keep track of/print just the four links with the highest traceability values
+        values[sourceFilename] = traceResult
+    return values
+    
+def outputValues(model, valuesDict, outputThreshold, curFile, output):
+    
+        for key in valuesDict:
+            if (float(valuesDict[key]) >= outputThreshold):
+                print("Source File: ",key, "Target File: ", curFile, "Traceability: ",valuesDict[key])
+                output.write("Model:"+ model + "\nSource File: " + key + ", Target File: " + curFile + ", Traceability: " + str(valuesDict[key]) + '\n')
+        
 
 os.chdir('../../')
 sourcePath = os.getcwd() + sys.argv[1]
 targetPath = os.getcwd()
 targetList = open(os.getcwd() + sys.argv[2], 'r', encoding='latin1').read().splitlines()
-outputThreshold = float(sys.argv[4]) 
-print(sys.argv[5])
-input = ast.literal_eval(sys.argv[5]) #the dictionary with (targetFile, sourceFile) -- a tuple -- as the key and the probability as the value
-#dictionary to be filled later
+threshold = float(sys.argv[4]) 
+feedbackSourceList = sys.argv[5].split(",")
+feedbackTargetList = sys.argv[6].split(",")
+feedbackNumList = sys.argv[7].split(",")
+input={} #dictionary to be filled
+for i in range (len(feedbackSourceList)):
+    input[feedbackSourceList[i]+" "feedbackTargetList[i]]=float(feedbackNumList[i]) #the dictionary with (targetFile, sourceFile) -- a tuple -- as the key and the probability as the value
 
 for targetFilename in targetList:
-    with open(os.path.join(targetPath, targetFilename), 'r', encoding='latin1') as f: # open in readonly mode
-        targetData = f.read()
-        f.close()
-    values = {}
-    for sourceFilename in os.listdir(sourcePath):
-        with open(os.path.join(sourcePath,sourceFilename), 'r', encoding='latin1') as f:
-            sourceData = f.read()
-            f.close()
 
-        result = facade.TraceLinkValue(sourceData,targetData,"word2vec")
-        traceResult = result[1]
-        tmpStr = targetFilename+" "+sourceFilename
-        if tmpStr in input:  #check if the file pair is in the input from the user
-            traceResult = new_probability(traceResult, float(input[tmpStr]))  #recalculating the probability
-        # print("Source File: ",sourceFilename, "Target File: ", targetFilename, "Traceability: ",result)
+    valuesWMD = traceabilityResult(sourcePath, targetPath, targetFilename, input, "word2vec", "WMD")
+    valuesSCM = traceabilityResult(sourcePath, targetPath, targetFilename, input, "word2vec", "SCM")
+    valuesDoc = traceabilityResult(sourcePath, targetPath, targetFilename, input, "doc2vec")
 
-    # new code added to keep track of/print just the four links with the highest traceability values
-        values[sourceFilename] = traceResult
+
+
     with open(os.getcwd() + sys.argv[3], 'w', encoding='latin1') as writeFile:
-        for key in values:
-            if (float(values[key]) >= outputThreshold):
-                print("Source File: ",key, "Target File: ", targetFilename, "Traceability: ",values[key])
-                writeFile.write("Source File: " + key + ", Target File: " + targetFilename + ", Traceability: " + str(values[key]) + '\n')
+
+        outputValues("word2vec, metric = WMD", valuesWMD, threshold, targetFilename, writeFile)
+        outputValues("word2vec, metric = SCM", valuesSCM, threshold, targetFilename, writeFile)
+        outputValues("doc2vec", valuesDoc, threshold, targetFilename, writeFile)
         writeFile.close()
